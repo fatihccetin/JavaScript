eadfc7362051aab74e66557bfdf50bef67ecb046
fatihccetin <fatih.ccetin2@gmail.com>
2021-01-25 00:44:02 +0300

    kucuk degısım

---
+++ script.js
@@ -0,0 +1,20 @@
+//var dısardın erişilebilir
+//let  tanımlandıgı sınıf ve metodlarda kullanılır.(degiştirilebilir)
+//const  tanımladıgın sınıfta kullanılır (degiştirilebilir)
+
+//fonksiyonlarda 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
